befd3789f96377300804d7c04ef9ed88b2d36494
Ryan Wade <rwade@dieselanalytics.com>
2016-11-07 09:50:51 -0500

    Update STEP_2.R

--- R_Scripts/STEP_2.R
+++ R_Scripts/STEP_2.R
@@ -1,4 +1,4 @@
-# The checkpoint library is used to make sure that we are using the most recent version of the packages as of the date that we pass
+# The checkpoint library is used to make sure that we are using the most recent version of the packages as of the date that we passed
 # to the "checkpoint" function. It scans all of the scripts in the working directory to perform the test and if the test fails 
 # "Checkpoint" will download the package that meets that criteria from MRAN.
 
