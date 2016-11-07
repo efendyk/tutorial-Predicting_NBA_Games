6d00245b4597fb02fe0edc2d7ef75a6f9c44bc97
Ryan Wade <rwade@dieselanalytics.com>
2016-11-07 09:59:00 -0500

    Update CombineGamePlayByPlayData.R

--- R_Scripts/CombineGamePlayByPlayData.R
+++ R_Scripts/CombineGamePlayByPlayData.R
@@ -3,7 +3,7 @@ library(readr)
 library(lubridate)
 
 
-setwd("C:/Users/rwade/Downloads/Delete/")
+setwd("<set this to the path that has the 1,176 files of the game data>")
 files <- list.files(".")
 fields.to.keep <- c("period", "team", "etype", "player", "points", "reason", "result", "steal", "type")
 
@@ -19,20 +19,20 @@ for (file in files)
 		at = substring(file, 10, 12)
 		ht = substring(file, 13, 15)
 		gd = ymd(substring(file, 1, 8))
-	    individual.gd$away_team <- at
-	    individual.gd$home_team <- ht
+	        individual.gd$away_team <- at
+	        individual.gd$home_team <- ht
 		individual.gd$game_date <- gd
 		total.gd <- individual.gd
 		first.pass = FALSE
    	} else
    	{
    		individual.gd <- read_csv(file)
-	    individual.gd <- individual.gd[,fields.to.keep]
+	        individual.gd <- individual.gd[,fields.to.keep]
 	 	at = substring(file, 10, 12)
 		ht = substring(file, 13, 15)
-	    gd = ymd(substring(file, 1, 8))
-	    individual.gd$away_team <- at
-	    individual.gd$home_team <- ht
+	        gd = ymd(substring(file, 1, 8))
+	        individual.gd$away_team <- at
+	        individual.gd$home_team <- ht
 		individual.gd$game_date <- gd
    		total.gd <- rbind(total.gd, individual.gd)
    	}
